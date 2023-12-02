395432985f19563465160af40af78f2976854c84
ayasyrev <a.yasyrev@gmail.com>
2023-12-02 13:36:02 +0300

    add time to metric

--- src/pttoolbox/train_loop.py
+++ src/pttoolbox/train_loop.py
@@ -1,9 +1,11 @@
 """Simple (base) pytorch train loop."""
+import time
 from dataclasses import dataclass, field
 from typing import Any, Callable, Literal
 
 import torch
 from accelerate import Accelerator
+from rich import print as rprint
 from rich.progress import Progress
 from timm.utils.metrics import AverageMeter, accuracy
 
@@ -14,7 +16,7 @@ class Cfg:
 
     epochs: int = 5
     lr: float = 0.001
-    # betas: tuple[float, float] = (0.95, 0.95)
+    # betas: tuple[float, float] = (0.95, 095)
     opt_cfg: dict[str, Any] = field(default_factory=dict)
 
 
@@ -47,6 +49,8 @@ def train_loop(
         "last_losses": [1.0] * num_last,
         "acc_val": AverageMeter(),
         "acc_train": AverageMeter(),
+        "time_train": [],
+        "time_validate": [],
     }
 
     def one_batch(xb: tuple[torch.Tensor, torch.Tensor]) -> dict[str, torch.Tensor]:
@@ -84,15 +88,16 @@ def train_loop(
             metrics["val_acc"].append(metrics["acc_val"].avg)
 
     def print_epoch_result(epoch: int):
-        print(
-            f"epoch {epoch + 1} {metrics['train_acc'][-1] / 100:0.2%} {metrics['losses'][-1]:0.3f}    {metrics['val_acc'][-1] / 100:0.2%} {metrics['val_losses'][-1]:0.3f}"
+        rprint(
+            f"epoch {epoch + 1} {metrics['train_acc'][-1] / 100:0.2%} {metrics['losses'][-1]:0.3f}"
+            f"    {metrics['val_acc'][-1] / 100:0.2%} {metrics['val_losses'][-1]:0.3f}"
+            f"    {metrics['time_train'][-1]:0.1f} / {metrics['time_validate'][-1]:0.1f} sec"
         )
 
     with Progress(transient=True) as progress:
         main_task = progress.add_task("", total=cfg.epochs)
         for epoch in range(cfg.epochs):
             progress.tasks[main_task].description = f"Epoch {epoch + 1} / {cfg.epochs}"
-
             result_reset()
             # train
             model.train()
@@ -110,13 +115,13 @@ def train_loop(
                     train_task
                 ].description = f"loss: {metrics['last_loss']:0.4f}"
 
+            metrics["time_train"].append(progress._tasks[train_task].finished_time)
             record_epoch("train")
             progress._tasks[
                 train_task
             ].description = (
                 f"{metrics['train_acc'][-1] / 100:.2%} {metrics['last_loss']:0.3f}"
             )
-
             # validate
             with torch.no_grad():
                 model.eval()
@@ -125,7 +130,7 @@ def train_loop(
                     one_batch(xb)
                     record_batch("validate")
                     progress.update(val_task, advance=1)
-
+            metrics["time_validate"].append(progress._tasks[val_task].finished_time)
             record_epoch("validate")
             print_epoch_result(epoch)
 
@@ -134,18 +139,4 @@ def train_loop(
             progress.update(main_task, advance=1)
 
         progress.remove_task(main_task)
-
     return metrics
-
-
-def print_result(metrics: dict[str, torch.Tensor]):
-    for item in zip(
-        range(1, len(metrics["train_acc"]) + 1),
-        metrics["train_acc"],
-        metrics["losses"],
-        metrics["val_acc"],
-        metrics["val_losses"],
-    ):
-        print(
-            f"ep {item[0]} {item[1] / 100:0.2%} {item[2]:0.3f}    {item[3] / 100:0.2%} {item[4]:0.3f}"
-        )
