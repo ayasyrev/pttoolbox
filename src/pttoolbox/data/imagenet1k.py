a9affb6647b404c356e927d87507216da7625956
ayasyrev <a.yasyrev@gmail.com>
2023-12-05 17:48:00 +0300

    train 1k

--- src/pttoolbox/data/imagenet1k.py
+++ src/pttoolbox/data/imagenet1k.py
@@ -5,7 +5,6 @@ from typing import Literal, Optional
 import pandas as pd
 
 
-
 def load_df(
     filename: Optional[str] = None,
     split: Optional[Literal["train", "val"]] = "val",
@@ -14,10 +13,11 @@ def load_df(
 
     Args:
         filename: path to parquet file. If no name is given, used prepared data.
-        split: split name, default: None
+        split: split name, default: val
     """
     if filename is None:
         filename = (
             resources.files("pttoolbox.data.data_info") / f"imagenet1k_{split}.parquet.gzip"
         )
+    print(filename)
     return pd.read_parquet(filename)
