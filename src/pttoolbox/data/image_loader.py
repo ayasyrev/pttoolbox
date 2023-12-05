cfa83a1800960f35fdb14f86fb11024806a2366d
ayasyrev <a.yasyrev@gmail.com>
2023-12-05 18:26:54 +0300

    loaders

---
+++ src/pttoolbox/data/image_loader.py
@@ -0,0 +1,24 @@
+# copied from torchvision and refactored
+import accimage
+from PIL import Image
+
+
+def pil_loader(path: str) -> Image.Image:
+    # open path as file to avoid ResourceWarning (https://github.com/python-pillow/Pillow/issues/835)
+    with open(path, "rb") as f:
+        img = Image.open(f)
+        return img.convert("RGB")
+
+
+def accimage_loader_safe(path: str) -> accimage.Image:
+
+    try:
+        return accimage.Image(path)
+    except OSError:
+        # Potentially a decoding problem, fall back to PIL.Image
+        return pil_loader(path)
+
+
+# @lru_cache(maxsize=None)
+def accimage_loader(path: str) -> accimage.Image:
+    return accimage.Image(path)
